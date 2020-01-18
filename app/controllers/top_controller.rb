c7e069120f2ae70b01ecd40de119344c6995c84c
risa <az.5s.fmt@gmail.com>
2020-01-18 21:39:13 +0900

    topページ作成(model/controller追加)

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,5 @@
+class TopController < ApplicationController
+    def index
+        @items = Item.all
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'top#index'
   get 'items', to: 'items#index'
   get 'carts', to: 'carts#index'
 end
